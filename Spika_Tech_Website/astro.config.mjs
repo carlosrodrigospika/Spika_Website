af581010969d2872ff71986889ea3471274eef10
Spika-Tech <107111360+carlosrodrigospika@users.noreply.github.com>
2025-10-17 08:47:06 +0200

    Latest updates

--- Spika_Tech_Website/astro.config.mjs
+++ Spika_Tech_Website/astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
       port: 4321,
       strictPort: true, // Falla si el puerto no está disponible
       allowedHosts: [
+        'bracelet-corrections-trend-passport.trycloudflare.com',
         'localhost',
         '127.0.0.1',
       ],
